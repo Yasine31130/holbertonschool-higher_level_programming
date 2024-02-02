1e228378cadbc086ec17f892787a0ece8b3b6cff
Yasine31130 <yasine.bns@hotmail.com>
2024-02-02 14:09:56 -0800

    Python - Exceptions

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    elements = 0
-    try:
-        for i in range(x):
-            print(my_list[i], end='')
-            elements += 1
-    except:
-        pass
+    count = 0
+    for element in my_list:
+        if count >= x:
+            break
+        try:
+            print(element, end="")
+            count += 1
+        except Exception:
+            break
     print()
-    return elements
+    return count
--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+
 def safe_print_integer(value):
-    if value == '89' or value == "89" or value == 89.9:
-        return False
     try:
-        print("{:d}".format(int(value)))
+        print("{:d}".format(value))
         return True
-    except:
+    except Exception:
         return False
