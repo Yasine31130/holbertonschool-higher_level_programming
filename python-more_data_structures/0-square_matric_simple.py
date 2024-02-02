42eed67e407bee88ecaa09f19a334908be9977ca
Yasine31130 <yasine.bns@hotmail.com>
2024-02-02 14:05:47 -0800

    Python - More Data Structures: Set, Dictionary

--- python-more_data_structures/0-square_matric_simple.py
+++ python-more_data_structures/0-square_matric_simple.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        for value in row:
-            new_row.append(value**2)
-        new_matrix.append(new_row)
-    return new_matrix
+    """
+    Computes the square value of all integers of a matrix.
+
+    Args:
+    - matrix: a 2-dimensional array
+
+    Returns:
+    - A new matrix:
+        - Same size as original matrix
+        - Each value should be the square of the value of the input
+    """
+    # Also works in one line but looks digusting :
+    # return list(map(lambda row: list(map(lambda x: x * x, row)), matrix))
+    return [[x * x for x in row] for row in matrix]
--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
-    if roman_string is None or not isinstance(roman_string, str):
+    """
+    Converts a Roman numeral to an integer.
+
+    Args:
+        roman_string (str): The Roman numeral to convert.
+
+    Returns:
+        int: Decimal notation of the Roman numeral,
+        or None if the input is invalid.
+    """
+    if (not roman_string or not isinstance(roman_string, str)):
+        # input is either None, or not a string ; returning default value
         return 0
-    romanDic = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-            }
-    result = 0
-    for i in range(len(roman_string)):
-        current = romanDic[roman_string[i]]
-        if i + 1 < len(roman_string) and romanDic[roman_string
-                [i + 1]] > current:
-            result -= current
+
+    # Simple translation table from roman numerals to decimals
+    table = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+
+    decimal = 0
+    length = len(roman_string)
+
+    for i in range(0, length):
+        # Ugly but had to respect 80-char limit
+        if ((i + 1) < length
+                and table[roman_string[i]] < table[roman_string[i + 1]]):
+            # Special case: subtract numeral, then keep going to next numeral
+            decimal -= table[roman_string[i]]
         else:
-            result += current
-    return result
+            # Usual case: find numeral value in the table and add to the sum
+            decimal += table[roman_string[i]]
+    return decimal
