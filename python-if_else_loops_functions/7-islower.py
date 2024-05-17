e883855da057346be9e47b95c521f72db0d6d931
Yasine31130 <yassine.benslika@holbertonstudents.com>
2024-05-17 09:29:43 -0700

    7. islower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    if c <= chr(122) and c >= chr(97):
-        return True
-    else:
-        return False
+    if c != (None):
+        if c <= chr(122) and c >= chr(97):
+            return True
+        else:
+            return False
