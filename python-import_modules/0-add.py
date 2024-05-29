77c1d66ea2596def0361ad744539c3be88062657
Yasine31130 <yassine.benslika@holbertonstudents.com>
2024-05-29 07:25:30 -0700

    0. Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-
+a = 1
+b = 2
 result = add(1, 2)
 
-print("1 + 2 = {}".format(result))
+print("{} + {} = {}".format(a, b, result))
